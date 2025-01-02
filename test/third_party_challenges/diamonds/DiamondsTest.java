9e04379d06c84e1e003240341298d077e6ec4b6d
jeffersontdm <jeffersontavares1611@gmail.com>
2025-01-02 13:32:58 -0300

    refact: refactor diamonds challenger

--- test/third_party_challenges/diamonds/DiamondsTest.java
+++ test/third_party_challenges/diamonds/DiamondsTest.java
@@ -7,17 +7,41 @@ import third_party_challenges.diamonds.solutions.Solution01;
 public class DiamondsTest {
 
   @Test
-  public void solution01Test() {
+  public void shouldReturnExpectedDiamondCutForFirstInput() {
     String diamonds01 = Solution01.diamondCut("**<**<**<**<**<**<**<**<**>>>>>>>>*****");
-    String diamonds02 = Solution01.diamondCut("<<<*<<**<<<<>*****>>**>>**>>>********>>");
-    String diamonds03 = Solution01.diamondCut("<*<*<*<*<*<*<*<*<*<*>*>*>*>*>*>*>*>*>*>");
-
     Assertions.assertEquals("<><><><><><><><>", diamonds01);
-    Assertions.assertEquals("<><><><><><><><><>", diamonds02);
-    Assertions.assertEquals("<><><><><><><><><><>", diamonds03);
+  }
 
-    Assertions.assertEquals(8, Solution01.diamondCount(diamonds01));
-    Assertions.assertEquals(9, Solution01.diamondCount(diamonds02));
-    Assertions.assertEquals(10, Solution01.diamondCount(diamonds03));
+  @Test
+  public void shouldReturnExpectedDiamondCutForSecondInput() {
+    String diamonds02 = Solution01.diamondCut("<<<*<<**<<<<>*****>>**>>**>>>********>>");
+    Assertions.assertEquals("<><><><><><><><><>", diamonds02);
+  }
+
+  @Test
+  public void shouldReturnExpectedDiamondCutForThirdInput() {
+    String diamonds03 = Solution01.diamondCut("<*<*<*<*<*<*<*<*<*<*>*>*>*>*>*>*>*>*>*>");
+    Assertions.assertEquals("<><><><><><><><><><>", diamonds03);
+  }
+
+  @Test
+  public void shouldReturnCorrectDiamondCountForFirstInput() {
+    String diamonds01 = Solution01.diamondCut("**<**<**<**<**<**<**<**<**>>>>>>>>*****");
+    Long count = Solution01.diamondCount(diamonds01);
+    Assertions.assertEquals(8, count);
+  }
+
+  @Test
+  public void shouldReturnCorrectDiamondCountForSecondInput() {
+    String diamonds02 = Solution01.diamondCut("<<<*<<**<<<<>*****>>**>>**>>>********>>");
+    Long count = Solution01.diamondCount(diamonds02);
+    Assertions.assertEquals(9, count);
+  }
+
+  @Test
+  public void shouldReturnCorrectDiamondCountForThirdInput() {
+    String diamonds03 = Solution01.diamondCut("<*<*<*<*<*<*<*<*<*<*>*>*>*>*>*>*>*>*>*>");
+    Long count = Solution01.diamondCount(diamonds03);
+    Assertions.assertEquals(10, count);
   }
 }
